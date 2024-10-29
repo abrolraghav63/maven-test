42e19c38ac2321c545e7e24ad8e7b719e8e3cdb1
Raghav Abrol <raghavab@amdocs.com>
2024-10-29 19:41:33 +0530

    source code

---
+++ src/main/java/com/raghav/firstproject/App.java
@@ -0,0 +1,10 @@
+package com.raghav.firstproject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
